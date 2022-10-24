ae8872f704a083cd24fcebaea7271276b7401ccd
mathbueeno <mathbueeno@gmail.com>
2022-10-24 10:17:45 -0300

    Update Repositórios

---
+++ SistemaDeTarefas/Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Task<List<Usuario>> BuscarTodosUsuarios();
+        Task<List<Usuario>> BuscarPorId(int id);
+        Task<Usuario> Adicionar(Usuario usuario);
+        Task<Usuario> Atualizar(Usuario usuario, int id);
+        Task<bool> Apagar(int id);
+
+    }
+}

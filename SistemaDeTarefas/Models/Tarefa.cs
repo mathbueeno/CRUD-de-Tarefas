b627e102af494a85880708d9fd4fca07db6c77de
mathbueeno <mathbueeno@gmail.com>
2022-10-24 09:45:19 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefas/Models/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaDeTarefas.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public int Status { get; set; }
+    }
+}
